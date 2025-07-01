d6460bf4f3df2029125224094d7ba55d66c6e856
AnamikaS2005 <tve22ec011@cet.ac.in>
2025-07-01 15:43:44 +0530

    Add files via upload

---
+++ tb_uart_recv.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.06.2025 15:43:56
+// Design Name: 
+// Module Name: tb_uart_recv
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_uart_recv(
+
+    );
+endmodule
